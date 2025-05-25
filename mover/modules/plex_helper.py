da9343519b232377d61011691154f3b8b0a17547
denys.kozhevnikov <github@mail.noonamer.com>
2025-05-25 10:02:17 +0100

    Continue watching init

--- mover/modules/plex_helper.py
+++ mover/modules/plex_helper.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import logging
-from typing import Any, Callable, Dict, Optional, Tuple, Union
+from typing import Dict
 from functools import cached_property
 
 class PlexHelper:
@@ -45,7 +45,7 @@ class PlexHelper:
             for item in section.search(unwatched=False):            
                 if item.type == 'movie':
                     __populate_watched(item)
-                elif section.type == 'show':
+                elif item.type == 'show':
                     for episode in item.episodes():
                        __populate_watched(episode)
                        
@@ -69,6 +69,31 @@ class PlexHelper:
 
     def is_watched(self, file: str) -> bool:
         return file in self.watched_media
+    
+    @cached_property
+    def continue_watching(self) -> set[str]:
+        result = set()
+        
+        def __populate_watching(item):
+            for media in item.media:
+                for part in media.parts:
+                    path = self.rewriter(part.file)
+                    if os.path.exists(path):
+                        logging.debug("Watching not on cache %s: %s (%s)", item.type, item.title, path)
+                        result.add(path)
+        
+        for item in sorted(self.__plex.continueWatching(), key=lambda i: i.lastViewedAt or 0, reverse=True):
+            if item.type == 'movie':
+                __populate_watching(item)
+            elif item.type == 'episode':
+                show = item.show()
+                key = (item.seasonNumber, item.index) if item.isWatched else (item.seasonNumber, item.index -1)
+                for episode in sorted([e for e in show.episodes() if (e.seasonNumber, e.index) > key], key=lambda e: (e.seasonNumber, e.index)):                    
+                    if episode.seasonNumber < item.seasonNumber and episode.index < item.index:
+                        continue
+                    __populate_watching(episode)
+                    
+        return result
     
     def __str__(self):
         return self.url
